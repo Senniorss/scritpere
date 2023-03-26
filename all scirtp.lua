0c2561a0b080ea1ef46d9a330180b5660a946e39
$eniors <121121650+Senniorss@users.noreply.github.com>
2023-03-26 16:04:55 +0100

    Add files via upload

---
+++ all scirtp.lua
@@ -0,0 +1,14 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruit
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
+elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 then
+    --King Legacy
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
+elseif placeId == 10982284336 then
+    --Anime Idle Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Senniorss/roblox-scripts/main/anime%20idle%20simulator/Protected.lua"))()
+elseif placeId == 11448052802 then
+	--Pet Rift
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Senniorss/roblox-scripts/main/pet%20rift/Protected.lua"))()
+end
